1915686d4fc4e04ffa363f4c025a6693d324ac4b
alex <plcgi1@gmail.com>
2025-12-22 17:47:29 +0300

    config

--- multiagent/src/config/envs/default.ts
+++ multiagent/src/config/envs/default.ts
@@ -30,7 +30,7 @@ export const defaultConfig: AppConfig = {
     baseUrl: process.env.OLLAMA_BASE_URL || "http://localhost:11434",
     model: process.env.OLLAMA_MODEL || "llama2",
     options: {
-      temperature: 0.1,
+      temperature: 0,
     },
   },
 
